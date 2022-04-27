a157a2552e7b8125af70a0cacb394e7e2225041f
prodo-developer <wjdals300@gmail.com>
2022-04-27 21:55:17 +0900

    feat : FrontControllerServletV4 Test

--- src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -33,7 +33,7 @@ public class FrontControllerServletV4 extends HttpServlet {
         String requestURI = request.getRequestURI();
 
         ControllerV4 controller = controllerMap.get(requestURI);
-        if(controller == null) {
+        if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
